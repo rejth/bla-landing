b3828ba0286464a295bd6bb810635e1674251366
Ilia Kirsanov <kirsanovia@yandex.ru>
2021-02-14 23:38:58 +0300

    added Header html

---
+++ src/components/Header/Header.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Header.scss';
+
+const Header = () => (
+  <header className="navbar">
+    <div className="container">
+      <div className="navbar__wrapper">
+        <a href="#" className="navbar__logo logo">
+          <img
+            className="navbar__logo-image"
+            src="img/b-logo.png"
+            alt="Image of logo"
+          />
+          <span className="navbar__title">BLA BLA</span>
+          <span className="navbar__subtitle">One Page Flat Template</span>
+        </a>
+        <ul className="navbar__list">
+          <NavbarLink target={'header'} text={'HOME'} />
+          <NavbarLink target={'portfolio'} text={'PORTFOLIO'} />
+          <NavbarLink target={'about'} text={'ABOUT'} />
+          <NavbarLink target={'contact'} text={'CONTACT'} />
+        </ul>
+      </div>
+    </div>
+  </header>
+);
+
+const NavbarLink = ({ target, text }) => (
+  <li className="navbar__item">
+    <a href={`#${target}`} className="navbar__link">
+      {text}
+    </a>
+  </li>
+);
+
+NavbarLink.propTypes = {
+  target: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+};
+
+export default Header;
