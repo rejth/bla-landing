50d2d126e949d72d5d15539c5234e100bb2d84f1
Ilia Kirsanov <kirsanovia@yandex.ru>
2021-02-15 23:26:20 +0300

    added style for Brand

--- src/components/Brand/Brand.js
+++ src/components/Brand/Brand.js
@@ -4,10 +4,10 @@ import './Brand.scss';
 const Brand = () => (
   <section className="brand">
     <h1 className="brand__title">We Build Brand</h1>
-    <h3 className="brand__subtitle">
+    <p className="brand__subtitle">
       Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy
       nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
-    </h3>
+    </p>
     <button className="brand__button">Learn more</button>
     <a href="#portfolio" className="brand__link">
       <img
