5a0e15469248177ae1ce0b5154d7bd5f163771e0
Ilia Kirsanov <kirsanovia@yandex.ru>
2021-02-16 23:42:05 +0300

    Updates

---
+++ js/icon_customImage.js
@@ -0,0 +1,41 @@
+setTimeout(() => {
+  const elem = document.createElement('script');
+  elem.type = 'text/javascript';
+  elem.src =
+    '//api-maps.yandex.ru/2.1.77/?load=package.standard&lang=ru-RU&onload=getYaMap';
+  document.getElementsByTagName('body')[0].appendChild(elem);
+}, 2000);
+
+function getYaMap() {
+  const myMap = new ymaps.Map(
+      'map',
+      {
+        center: [7.838252, 98.29922],
+        zoom: 9,
+      },
+      {
+        searchControlProvider: 'yandex#search',
+      }
+    ),
+    myPlacemark = new ymaps.Placemark(
+      myMap.getCenter(),
+      {
+        hintContent: 'Grand Hotel',
+        balloonContent: 'The awesome place in the world!',
+      },
+      {
+        // Опции.
+        // Необходимо указать данный тип макета.
+        iconLayout: 'default#image',
+        // Своё изображение иконки метки.
+        iconImageHref: './img/myIcon.gif',
+        // Размеры метки.
+        iconImageSize: [30, 42],
+        // Смещение левого верхнего угла иконки относительно
+        // её "ножки" (точки привязки).
+        iconImageOffset: [-5, -38],
+      }
+    );
+
+  myMap.geoObjects.add(myPlacemark);
+}
