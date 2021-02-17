2acf25a391ed2373b3e1776f0bce40365ecc4e10
Ilia Kirsanov <kirsanovia@yandex.ru>
2021-02-18 01:38:44 +0300

    some fix

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -48,7 +48,7 @@ const Contact = () => (
               width="100%"
               height="265"
               frameBorder="0"
-              style="border: 0"
+              style={{ border: 0 }}
               allowFullScreen=""
               aria-hidden="false"
               tabIndex="0"
