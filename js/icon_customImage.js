5d0322efc6e4630a938910e0e1ded2edb98d2534
Ilia Kirsanov <kirsanovia@yandex.ru>
2021-02-17 23:12:54 +0300

    Updates

--- js/icon_customImage.js
+++ js/icon_customImage.js
@@ -8,34 +8,57 @@ setTimeout(() => {
 
 function getYaMap() {
   const myMap = new ymaps.Map(
-      'map',
-      {
-        center: [55.798682, 37.695816],
-        zoom: 9,
-      },
-      {
-        searchControlProvider: 'yandex#search',
-      }
-    ),
-    myPlacemark = new ymaps.Placemark(
-      myMap.getCenter(),
-      {
-        hintContent: 'Bla Bla Company 125009 Moscow, Red Square 1',
-        balloonContent: 'The awesome place in the world!',
-      },
-      {
-        // Опции.
-        // Необходимо указать данный тип макета.
-        iconLayout: 'default#image',
-        // Своё изображение иконки метки.
-        iconImageHref: '../img/myIcon.gif',
-        // Размеры метки.
-        iconImageSize: [30, 42],
-        // Смещение левого верхнего угла иконки относительно
-        // её "ножки" (точки привязки).
-        iconImageOffset: [-5, -38],
-      }
-    );
+    'map',
+    {
+      center: [55.798682, 37.695816],
+      zoom: 9,
+      controls: ['routePanelControl'],
+    },
+    {
+      searchControlProvider: 'yandex#search',
+    }
+  );
+
+  // Построение маршрута.
+  // По умолчанию строится автомобильный маршрут.
+  const multiRoute = new ymaps.multiRouter.MultiRoute(
+    {
+      // Точки маршрута. Точки могут быть заданы как координатами, так и адресом.
+      referencePoints: [
+        'Москва, метро Преображенская площадь',
+        'Москва, метро Сокольники',
+        'Колодезный переулок, 2А',
+      ],
+    },
+    {
+      // Автоматически устанавливать границы карты так,
+      // чтобы маршрут был виден целиком.
+      boundsAutoApply: true,
+    }
+  );
+
+  // Добавление маршрута на карту.
+  myMap.geoObjects.add(multiRoute);
+
+  const myPlacemark = new ymaps.Placemark(
+    myMap.getCenter(),
+    {
+      hintContent: 'Bla Bla Company 125009 Moscow, Red Square 1',
+      balloonContent: 'The awesome place in the world!',
+    },
+    {
+      // Опции.
+      // Необходимо указать данный тип макета.
+      iconLayout: 'default#image',
+      // Своё изображение иконки метки.
+      iconImageHref: '../img/map/myIcon.gif',
+      // Размеры метки.
+      iconImageSize: [30, 42],
+      // Смещение левого верхнего угла иконки относительно
+      // её "ножки" (точки привязки).
+      iconImageOffset: [-5, -38],
+    }
+  );
 
   myMap.geoObjects.add(myPlacemark);
 }
