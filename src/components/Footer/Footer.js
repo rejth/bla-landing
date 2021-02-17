89a616039a4bbc2e39e9183bbd88107947728a93
Ilia Kirsanov <kirsanovia@yandex.ru>
2021-02-17 22:03:46 +0300

    some fix

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,10 +5,12 @@ import './Footer.scss';
 const Footer = () => (
   <footer className="footer">
     <div className="container">
-      <div className="footer__info">
-        © Copyright 2016 Bla Bla Studio | One Page Flat Template
+      <div className="footer__wrapper">
+        <div className="footer__info">
+          © Copyright 2016 Bla Bla Studio | One Page Flat Template
+        </div>
+        <Shevron />
       </div>
-      <Shevron />
     </div>
   </footer>
 );
