4e559f5ab6d41596b6f8967170f837fbee2aaf04
Ilia Kirsanov <kirsanovia@yandex.ru>
2021-02-18 01:04:11 +0300

    Updates

--- js/icon_customImage.js
+++ js/icon_customImage.js
@@ -1,43 +1,33 @@
-setTimeout(() => {
-  const elem = document.createElement('script');
-  elem.type = 'text/javascript';
-  elem.src =
-    '//api-maps.yandex.ru/2.1.77/?load=package.standard&lang=ru-RU&onload=getYaMap';
-  document.getElementsByTagName('body')[0].appendChild(elem);
-}, 2000);
-
-function getYaMap() {
+ymaps.ready(() => {
   const myMap = new ymaps.Map(
-    'map',
-    {
-      center: [55.798682, 37.695816],
-      zoom: 9,
-      controls: ['routePanelControl'],
-    },
-    {
-      searchControlProvider: 'yandex#search',
-    }
-  );
-
-  const myPlacemark = new ymaps.Placemark(
-    myMap.getCenter(),
-    {
-      hintContent: 'Bla Bla Company 125009 Moscow, Red Square 1',
-      balloonContent: 'The awesome place in the world!',
-    },
-    {
-      // Опции.
-      // Необходимо указать данный тип макета.
-      iconLayout: 'default#image',
-      // Своё изображение иконки метки.
-      iconImageHref: './img/map/myIcon.gif',
-      // Размеры метки.
-      iconImageSize: [30, 42],
-      // Смещение левого верхнего угла иконки относительно
-      // её "ножки" (точки привязки).
-      iconImageOffset: [-5, -38],
-    }
-  );
+      'map',
+      {
+        center: [7.838252, 98.29922],
+        zoom: 9,
+      },
+      {
+        searchControlProvider: 'yandex#search',
+      }
+    ),
+    myPlacemark = new ymaps.Placemark(
+      myMap.getCenter(),
+      {
+        hintContent: 'Grand Hotel',
+        balloonContent: 'The awesome place in the world!',
+      },
+      {
+        // Опции.
+        // Необходимо указать данный тип макета.
+        iconLayout: 'default#image',
+        // Своё изображение иконки метки.
+        iconImageHref: './img/myIcon.gif',
+        // Размеры метки.
+        iconImageSize: [30, 42],
+        // Смещение левого верхнего угла иконки относительно
+        // её "ножки" (точки привязки).
+        iconImageOffset: [-5, -38],
+      }
+    );
 
   myMap.geoObjects.add(myPlacemark);
-}
+});
