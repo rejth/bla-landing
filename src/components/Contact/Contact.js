809c68335d34b9760091e3ac9b6113504202a5f5
Ilia Kirsanov <kirsanovia@yandex.ru>
2021-02-18 23:36:52 +0300

    some fix

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -38,10 +38,12 @@ const Contact = () => (
           <button className="contact__button">SUBMIT</button>
         </form>
         <div className="contact__map">
-          <i className="fa fa-map-marker-alt"></i>
-          <span className="contact__location">
-            Bla Bla Company 125009 Moscow, Red Square 1
-          </span>
+          <div className="contact__marker">
+            <i className="fa fa-map-marker-alt"></i>
+            <span className="contact__location">
+              Bla Bla Company 125009 Moscow, Red Square 1
+            </span>
+          </div>
           <div id="map">
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2242.7823244714937!2d37.695927716055834!3d55.79701759604827!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46b535a128b3e401%3A0xbd8e99fb0dffe2fe!2z0JrQvtC70L7QtNC10LfQvdGL0Lkg0L_QtdGALiwgMtCwLCDQnNC-0YHQutCy0LAsIDEwNzA3Ng!5e0!3m2!1sru!2sru!4v1613601840966!5m2!1sru!2sru"
